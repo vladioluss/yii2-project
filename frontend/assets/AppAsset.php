3efbcee58d5cdf54b4453f4a20ac1e37470d5e47
sadraccoonx <grishin1999@list.ru>
2022-04-17 18:22:48 +0300

    [upd] Обновлена структура

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+
+    public $css = [
+        'css/animate.css',
+        'css/classy-nav.min.css',
+        'css/font-awesome.min.css',
+        'css/jquery-ui.min.css',
+        'css/magnific-popup.css',
+        'css/nice-select.css',
+        'css/owl.carousel.css',
+        //'css/bootstrap.min.css'
+    ];
+
+    public $js = [
+        'js/active.js',
+        'js/classy-nav.min.js',
+        'js/map-active.js',
+        'js/plugins.js',
+        'js/popper.min.js',
+        //'js/bootstrap.min.js'
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset'
+    ];
+}
